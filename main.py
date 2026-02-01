da8a2df4d3f39c549136b17ac2789ff4b9185956
SyFord421 <jaki05118@gmail.com>
2026-02-01 19:04:51 +0700

    Sekarang sudah bisa mendapatkan data dari openweathermap

--- main.py
+++ main.py
@@ -2,15 +2,16 @@ import telebot
 from config import key
 import requests
 
-
 class BaseBot:
+    """inisiasi class utama"""
     def __init__(self, bot_api, id_chat, weather_api, city):
-        self.id_chat = bot_api
+        self.id_chat = id_chat
         self.bot = telebot.TeleBot(bot_api)
-        self.weather_token = weather_token
+        self.weather_token = weather_api
         self.city = city
 
     def send_message(self, text):
+        """fungsi untuk mengirimkan pesan dengan library telebot dan error handling"""
         try:
             self.bot.send_message(self.id_chat, text)
             print("[√] Pesan Telah Terkirim ")
@@ -18,17 +19,21 @@ class BaseBot:
             print(f"[!] Error {e}")
 
     def get_weather_info(self):
+        """"Mengambil data cuaca dari OpenWeatherMap API dan mengirimkannya ke Telegram.""" 
         try:
             url = f"http://api.openweathermap.org/data/2.5/weather?q={self.city}&appid={self.weather_token}&units=metric&lang=id"
-            weatherdata = requests.get(url).json()
+            #&lang=id agar data yang di kirim bebahasa indonnesia
+            weatherdata = requests.get(url).json()#Mengubah response mentah dari API menjadi dictionary Python agar mudah diolah.
             if weatherdata.get("cod") != 200:
-                print("[!] Error Kota Tidak ditemukan")
+                print(f"[!] Error: {weatherdata.get('message', 'Kota tidak ditemukan')}")
+                return
             weather = weatherdata['weather'][0]['main']
             desc = weatherdata['weather'][0]['description']
             temp = weatherdata['main']['temp']
-            self.send_message(f"---- Forecast Hari ini---\n 📍 Kota : {self.city}\n ☁ Cuaca : {weather}\n 🌡 Suhu : {temp}°C\n 📄 deskripsi : {desc}"
+            msg = (f"---- Forecast Hari ini---\n 📍 Kota : {self.city}\n ☁ Cuaca : {weather}\n 🌡 Suhu : {temp}°C\n 📄 deskripsi : {desc}")
+            self.send_message(msg)
         except Exception as e:
-            print("[!] Error Tidak dapat mendapatakan data")
+            print(f"[!] Error Tidak dapat mendapatakan data {e}")
 
 if __name__ == "__main__":
     BB = BaseBot(key['BOT_TELEGRAM'], key['ID_CHAT'], key['API_CUACA'], key['KOTA'])
