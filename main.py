c95607728d0aab58006b163d0cabfc3d8a82d78b
SyFord421 <jaki05118@gmail.com>
2026-04-13 20:15:57 +0700

    fix: Refactoring and Changing data provider to bmkg

--- main.py
+++ main.py
@@ -1,52 +1,67 @@
-import telebot
+import telebot.async_telebot
 from config import key
-import requests
+import aiohttp
+import asyncio
 
 class BaseBot:
     """inisiasi class utama"""
-    def __init__(self, bot_api, id_chat, weather_api, city):
-        self.id_chat = id_chat
-        self.bot = telebot.TeleBot(bot_api)
-        self.weather_token = weather_api
-        self.city = city
-
-    def send_message(self, text):
+    def __init__(self):
+        self.id_chat = key['ID_CHAT']
+        self.bot = telebot.async_telebot.AsyncTeleBot(key['BOT_TELEGRAM'])
+        
+    async def send_message(self, text:str):
         """fungsi untuk mengirimkan pesan dengan library telebot dan error handling"""
         try:
-            self.bot.send_message(self.id_chat, text)
+            await self.bot.send_message(self.id_chat, text)
             print("[√] Pesan Telah Terkirim ")
         except Exception as e:
             print(f"[!] Error {e}")
-
-    def get_weather_info(self):
-        """"Mengambil data cuaca dari OpenWeatherMap API dan mengirimkannya ke Telegram.""" 
+            
+class Weather_Bot(BaseBot):
+    def __init__(self, regional_code: str, city_name: str):
+        super().__init__()
+        self.regional_code = regional_code
+        self.city_name = city_name
+        
+        
+    async def get_weather_info(self) -> tuple[str, float] | None:
+        """"Mengambil data cuaca dari API dan mengirimkannya ke Telegram.""" 
         try:
-            url = f"http://api.openweathermap.org/data/2.5/weather?q={self.city}&appid={self.weather_token}&units=metric&lang=id"
+            url = f"https://api.bmkg.go.id/publik/prakiraan-cuaca?adm4={self.regional_code}"
             #&lang=id agar data yang di kirim bebahasa indonnesia
-            weatherdata = requests.get(url).json()
-            if weatherdata.get("cod") != 200:
-                print(f"[!] Error: {weatherdata.get('message', 'Kota tidak ditemukan')}")
-                return
-            weather = weatherdata['weather'][0]['main']
-            desc = weatherdata['weather'][0]['description']
-            temp = weatherdata['main']['temp']
-            return weather, desc, temp
+            async with aiohttp.ClientSession() as session:
+                async with session.get(url) as response:
+                    data = await response.json()
+            desc = data['data'][0]['cuaca'][0][0]['weather_desc']
+            temp = data['data'][0]['cuaca'][0][0]['t']
+            return desc, temp 
         except Exception:
-            return None, None, None
+            return None
 
-    def full_report(self):
+
+    async def full_report(self):
         try:
-            weather, desc, temp = self.get_weather_info()
+            desc, temp = await self.get_weather_info()
+            if temp is None:
+                return
             if temp <= 22:
-                message = "🥶 Dingin banget jangan lupa pake hoodie yah biar nggak kedinginan "
+                message = "🥶 Dingin banget, pake hoodie ya!"
             elif temp >= 29:
-                message = "🌤 cuaca-nya panas banget hari ini jangan lupa banyakin minum air putih biar nggak dehidrasi"
+                message = "🌤 Panas banget, jangan dehidrasi!"
             else:
                 message = "Suhu yang pas untuk jalan-jalan 🍃"
-            self.send_message(f"\n ☁ Local Forecast ☁ \n📍 Kota: {self.city}\n ☁ Cuaca  : {desc.capitalize()}\n🌡 Suhu : {temp}°C\n {message}")
+            report = (f"\n☁ Local Forecast ☁\n"
+                      f"📍 Lokasi: {self.city_name}\n"
+                      f"☁ Cuaca: {desc}\n"
+                      f"🌡 Suhu: {temp}°C\n"
+                      f"{message}")
+            
+            await self.send_message(report)
+            await asyncio.sleep(0.1)
         except Exception as e:
-            self.send_message(f"Error:\n{e}")
+            await self.send_message(f"Error Report:\n{e}")
+
 
 if __name__ == "__main__":
-    BB = BaseBot(key['BOT_TELEGRAM'], key['ID_CHAT'], key['API_CUACA'], key['KOTA'])
-    BB.full_report()
+    bot = Weather_Bot(key['regional_code'], "Padaasih")
+    asyncio.run(bot.full_report())
